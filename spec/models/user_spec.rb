68d6ae2204ad01a1986520449de7aa73102b834b
lukyans <sukyans@gmail.com>
2017-04-12 16:54:18 -0600

    create checkout

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
+RSpec.describe User, type: :model do
   context "validations" do
-    it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:username) }
+    it { is_expected.to validate_presence_of(:password) }
   end
 end
