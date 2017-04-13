7d172353c5eb5bd9a3468457ff52f5ec012afbda
NessEFC <NessEFC@gmail.com>
2017-04-12 20:08:46 -0600

    update seeds file

--- db/seeds/production.rb
+++
@@ -1,9 +0,0 @@
-Escape.create(name: "Gentle Dawn", description: "The gentlest of dawns.", city: "Denver", price: 200.00, image: "gentle_dawn.jpg")
-
-Escape.create(name: "Basement Dweller", description: "Enjoy the fluorescent lighting!", city: "Denver", price: 1.00, image: "basement_dweller.jpg")
-
-Category.create(title: "Outdoors")
-Category.create(title: "Indoors")
-
-EscapeCategory.create(category_id: 1, escape_id: 1)
-EscapeCategory.create(category_id: 2, escape_id: 2)
