62b556cccc6a3e5b6cc2f5c5f60363541cb3c49a
lukyans <sukyans@gmail.com>
2017-04-12 19:12:12 -0600

    add Order, OrderEscape and User  models  validation test
    Start writing test for user story 10

---
+++ spec/features/user_can_view_past_orders_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature "As a visitor" do
+  scenario "they can view past orders" do
+
+#USER STORY 10
+# An existing user that has one previous order
+# As an authenticated user
+# When I visit "/orders"
+# Then I should see my past order
+# And I should see a link to view that order
+# And when I click that link
+# Then I should see each item that was order with the quantity and line-item subtotals
+# And I should see links to each item's show page
+# And I should see the current status of the order (ordered, paid, cancelled, completed)
+# And I should see the total price for the order
+# And I should see the date/time that the order was submitted
+# If the order was completed or cancelled
+# Then I should see a timestamp when the action took place
+  end
+end
---
+++ spec/models/order_escape_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe OrderEscape, type: :model do
+  context "validations" do
+    it { is_expected.to validate_presence_of(:total) }
+    it { is_expected.to validate_presence_of(:quantity) }
+  end
+end
+
+RSpec.describe OrderEscape, "associations", type: :model do
+  it { should belongs_to(:order) }
+  it { should belongs_to(:escape) }
+end
