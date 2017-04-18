c1630ef91b13d85f9340c8f487d1e5e90527070c
lukyans <sukyans@gmail.com>
2017-04-18 16:03:38 -0600

    dashboard style is comlete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170417194227) do
+ActiveRecord::Schema.define(version: 20170418200740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(version: 20170417194227) do
     t.integer  "role",                  default: 0
     t.string   "full_name"
     t.string   "address"
+    t.string   "image"
   end
 
   add_foreign_key "escape_categories", "categories"
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,20 @@ Escape.create(name: "Basement Dweller", description: "Enjoy the fluorescent ligh
 
 Escape.create(name: "The Old Folks Home", description: "Why not visit grandpa?", city: "Tampa Bay", price: 599.99, image: "grandpa.jpg", active: false)
 
+Escape.create(name: "A week in Hospital", description: "Comfortable bed and three meals a day", city: "Miami", price: 1099.99, image: "hospital.jpg")
+
+Escape.create(name: "Spend Intermission in a Movie Theater", description: "Get one ticket and stay in a movie theater for the whole week", city: "St. Louis", price: 12.99, image: "theater.jpg")
+
 Category.create(title: "Outdoors")
 Category.create(title: "Indoors")
 
 EscapeCategory.create(category_id: 1, escape_id: 1)
 EscapeCategory.create(category_id: 2, escape_id: 2)
 EscapeCategory.create(category_id: 2, escape_id: 3)
+EscapeCategory.create(category_id: 2, escape_id: 4)
+EscapeCategory.create(category_id: 2, escape_id: 5)
 
-User.create(username: "iadmin", password: "ipass", password_confirmation: "ipass", role: 1, full_name: "Admin Smith", address: "321 Broadway Ave, Denver CO 90210")
+User.create(username: "iadmin", password: "ipass", password_confirmation: "ipass", role: 1, full_name: "Admin Smith", address: "321 Broadway Ave, Denver CO 90210", image: "iadmin.jpg")
 User.create(username: "booradley", password: "pass", password_confirmation: "pass", full_name: "Booseph Radley III", address: "111 Mockingbird Lane, Phoenix AZ 90000")
 
 User.last.orders.create(status: "paid", total: 200.00)
