fcc7b07a8c62e3638aa92ed0ec3f32eb43b5467e
caward12 <ward.colleen.a@gmail.com>
2017-04-19 16:13:08 -0600

    refactor controllers and some styling

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,31 +1,35 @@
 class CartsController < ApplicationController
+  before_action :set_escape, only: [:create, :update, :destroy]
+
   def show
     @escapes = @cart.find_escapes
   end
 
   def create
-    escape = Escape.find(params[:escape_id])
-    @cart.add_item(escape.id)
+    @cart.add_item(@escape.id)
     session[:cart] = @cart.contents
     redirect_to root_path
   end
 
   def update
-    escape = Escape.find(params[:escape_id])
     if params[:quantity] == "decrease"
-      @cart.decrease_quantity(escape.id)
+      @cart.decrease_quantity(@escape.id)
     else
-      @cart.increase_quantity(escape.id)
+      @cart.increase_quantity(@escape.id)
     end
     redirect_to cart_path
   end
 
   def destroy
-    escape = Escape.find(params[:escape_id])
     @cart.remove_item(params[:escape_id])
-    flash[:success] = "Successfully removed #{view_context.link_to(escape.name, escape_path(escape))} from your cart."
+    flash[:success] = "Successfully removed #{view_context.link_to(@escape.name, escape_path(@escape))} from your cart."
     redirect_to cart_path
   end
 
+private
+
+  def set_escape
+    @escape = Escape.find(params[:escape_id])
+  end
 
 end
