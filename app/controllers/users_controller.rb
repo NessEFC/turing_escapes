b1462b47f801af9ff0c18c013925f58d7192bc66
lukyans <sukyans@gmail.com>
2017-04-19 19:48:43 -0600

    fix some images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation)
+    params.require(:user).permit(:full_name, :address, :username, :password, :password_confirmation)
   end
 end
